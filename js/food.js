ed445da52676e91fb902baec4f81e8942d263668
altair4ik <shkabara.alexandr@gmail.com>
2017-01-19 18:50:44 +0200

    update vibro and smoke (create snoke brightness) rename rotate

--- js/food.js
+++ js/food.js
@@ -1,54 +1,56 @@
 var steakImages;
 
 function Steak() {
-    var rotateCheck = false;
-
+    var fryCheck = false;
     this.create = function () {
         $('body').append("<div class='steak'>" +
-            "<div class='steak-vibro'>" +
-                "<img src='img/smoke.png' class='steak-img smoke'>" +
+            "<div class='steak-images'>" +
                 "<img src='img/steak.png' class='steak-img food-first-img'>" +
                 "<img src='img/steak2.png' class='steak-img food-second-img'>" +
                 "<img src='img/steak3.png' class='steak-img food-thread-img'>" +
                 "<img src='img/steak4.png' class='steak-img food-fourth-img'>" +
-            "</div>>"+
+            "</div>"+
+                "<div class='smoke-wrapper'>"+
+                "<img src='img/smoke.png' class='steak-img smoke'>" +
+                "</div>" +
             "</div>");
         steakImages = $('.steak-img');
-        $(steakImages[1]).animate({opacity : '1'},3000)
+        $(steakImages[0]).animate({opacity : '1'},3000)
     };
     this.goToGrill = function () {
         $('.steak').animate({'top' : '210px', 'left' : '210px'}, 1000);
     };
     this.fry = function () {
-        if(rotateCheck) {
+        if(fryCheck) {
             $(steakImages[2]).animate({opacity: '0'}, 10000);
             $(steakImages[3]).animate({opacity: '1'}, 1);
         } else {
             $(steakImages[0]).animate({opacity: '0'}, 10000);
             $(steakImages[1]).animate({opacity: '1'}, 1);
+            return fryCheck = true;
         }
     };
 
-    this.rotateSteak = function () {
+    this.rotate = function () {
         $('.steak').css('transform', 'rotate(180deg)');
         $(steakImages[1]).css('opacity', '0');
         $(steakImages[2]).css('opacity', '1');
-
-        return rotateCheck = true;
     };
 
     this.vibrateOn = function () {
-        $('.steak-vibro').jrumble();
-        $('.steak-vibro').trigger('startRumble');
+        $('.steak-images').jrumble();
+        $('.steak-images').trigger('startRumble');
+        $('.smoke').css('filter', 'brightness(0)');
     };
 
     this.vibrateOff = function () {
-        $('.steak-vibro').trigger('stopRumble');
+        $('.steak-images').trigger('stopRumble');
     };
 
     this.smokeOn = function () {
         $('.smoke').animate({'opacity' : '1'}, 1000);
         $('.smoke').animate({'filter' : 'brightness(0)'}, 3000);
+        $('.smoke').css('filter', 'brightness(1)');
     };
 
     this.smokeOff = function () {
