5b0c9f871140f4c70707646595d3486bc42c803e
altair4ik <shkabara.alexandr@gmail.com>
2017-01-19 15:12:16 +0200

    create Steak(create function - create, fry, rotateSteak, vibro...)

--- js/food.js
+++ js/food.js
@@ -1,17 +1,52 @@
-var steak = $('.steak');
+var steakImages;
 
 function Steak() {
+    var rotateCheck = false;
+
     this.create = function () {
-        $('body').append("<div class='steak'><img src='img/steak.png' class='steak'>");
-        steak = $('.steak');
-    }
+        $('body').append("<div class='steak'>" +
+            "<img src='img/steak.png' class='steak-img food-first-img'>" +
+            "<img src='img/steak2.png' class='steak-img food-second-img'>" +
+            "<img src='img/steak3.png' class='steak-img food-thread-img'>" +
+            "<img src='img/steak4.png' class='steak-img food-fourth-img'>" +
+            "</div>");
+        steakImages = $('.steak-img');
+        $(steakImages[0]).animate({opacity : '1'},3000)
+    };
     this.go = function () {
 
-        $(steak[0]).animate({top : '230px', left : '230px'}, 5000);
-        steak = $('.steak');
-    }
+    };
     this.fry = function () {
-        $(steak[0]).attr('src', 'img/steak2.png');
-        steak = $('.steak');
+        if(rotateCheck) {
+            $(steakImages[2]).animate({opacity: '0'}, 10000);
+            $(steakImages[3]).animate({opacity: '1'}, 1);
+        } else {
+            $(steakImages[0]).animate({opacity: '0'}, 10000);
+            $(steakImages[1]).animate({opacity: '1'}, 1);
+        }
+    };
+
+    this.rotateSteak = function () {
+        $('.steak').css('transform', 'rotate(180deg)');
+        $(steakImages[1]).css('opacity', '0');
+        $(steakImages[2]).css('opacity', '1');
+
+
+        return rotateCheck = true;
+    };
+
+    this.vibrateOn = function () {
+        $('.steak').addClass('vibro');
+    };
+
+    this.vibrateOff = function () {
+        $('.steak').removeClass('vibro');
+    };
+    this.out = function () {
+
+    };
+
+    this.destroy = function () {
+
     }
 }
